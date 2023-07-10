deaa46b51b4fd9c0802f8565a65bc27a60b01cbf
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 02:20:54 +0800

    Fan Pseudocode

---
+++ Fan Class/Fan_Class.py
@@ -0,0 +1,8 @@
+# Mohammad Mishal S. Noroña | BSCPE 1-5 | Fan Class
+# Create a class
+# Add Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+# Create a Constructor
+# Speed Function
+# Status Function
+# Radius Function
+# Color Function
