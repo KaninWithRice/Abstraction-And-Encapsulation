3e4a6bb14d2626c843333fb21b04174d37f14215
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 03:38:26 +0800

    Two Fan Objects

--- Fan Class/Test_Fan.py
+++ Fan Class/Test_Fan.py
@@ -1,6 +1,17 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Fan Class
 # Import Colorama
-# Introduction
+import colorama
+from colorama import Fore, Back, Style
+colorama.init()
+# Import Class
+from Fan_Class import Fan
 # Create a Loop
+def start():
+# Introduction
+    print("")
+    print(Fore.CYAN + "WELCOME TO FAN GENERATOR".center(40," ") )
+    print(Fore.YELLOW + "By: Mishal Noroña".center(40," ") + Fore.WHITE )
 # Create Two Fan Object
+    fan1 = Fan(Fan.FAST, "Yellow", 10, True)
+    fan1 = Fan(Fan.SLOW, "Blue", 5, False)
 # Loading Screen
