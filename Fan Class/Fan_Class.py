57bad20971cfbbb1585c2c3bec09512e7e1de72b
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 02:24:51 +0800

    Three Constant

--- Fan Class/Fan_Class.py
+++ Fan Class/Fan_Class.py
@@ -1,6 +1,10 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Fan Class
 # Create a class
+class Fan:
 # Add Three constants named SLOW, MEDIUM, and FAST with the values 1, 2, and 3 to denote the fan speed.
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
 # Create a Constructor
 # Speed Function
 # Status Function
