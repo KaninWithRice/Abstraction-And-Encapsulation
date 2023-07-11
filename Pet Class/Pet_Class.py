3f1d756eaf4f4eb0d939965166bbc7da28fc6ec1
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 23:59:02 +0800

    Constructor

--- Pet Class/Pet_Class.py
+++ Pet Class/Pet_Class.py
@@ -1,6 +1,11 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Pet Class
 # Create a Class
+class Pet:
 # Create a Constructor
+    def __init__ (pet, name, type, age):
+        pet.__name = name
+        pet.__type = type
+        pet.__age = age
 # Name Function
 # Animal Type Function
 # Age Function
