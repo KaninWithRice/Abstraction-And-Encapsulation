dd8d1573e519e45f7ff8a1c8ded5740372257a7c
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 21:25:50 +0800

    Test file is not working

--- Car Class/Car_Class.py
+++ Car Class/Car_Class.py
@@ -2,10 +2,14 @@
 # Create a Class
 class Car:
 # Create attributes Year Model, Make and Speed
-    def __init__ (car, yrmodel, make, speed):
+    def __init__ (car, yrmodel = 2019, make = "Honda", speed = 0):
         car.__yrmodel = yrmodel
         car.__make = make
-        car.__speed = 0
+        car.__speed = speed
+    def yrmodel (car):
+        return car.__yrmodel
+    def make (car):
+        return car.__make
 # Accelaration Function
     def acceleration (car):
         car.__speed += 5
--- Car Class/Test_Car.py
+++ Car Class/Test_Car.py
@@ -1,5 +1,12 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Car Class
 # Import Class
+from Car_Class import Car
 # Create an Object
+Car1 = Car()
 # Car Acceleration
+for acc in range(5):
+    Car1.yrmodel()
+    Car1.make()
+    Car1.speed()
+    print("The car is now at : ", Car1.acceleration)
 # Car Brake
