8db0cdb88bb37f9453155b4497a3243eae52f708
KaninWithRice <mishalnorona014@gmail.com>
2023-07-11 15:17:07 +0800

    Attributes Added

--- Car Class/Car_Class.py
+++ Car Class/Car_Class.py
@@ -1,6 +1,11 @@
 # Mohammad Mishal S. Noroña | BSCPE 1-5 | Car Class
 # Create a Class
+class Car:
 # Create attributes Year Model, Make and Speed
+    def __init__ (car, yrmodel, make, speed):
+        car.__yrmodel = yrmodel
+        car.__make = make
+        car.__speed = 0
 # Accelaration Function
 # Brake Function
 # Speed Function
